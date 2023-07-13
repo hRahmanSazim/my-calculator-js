42960f9eadb69c602067b06d05849abc87b1b61b
Hasibur Rahman <hasibur.rahman@sazim.io>
2023-07-13 17:28:51 +0600

    add keyboard functionality

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-const display = document.querySelector(".display")
-const buttons = document.querySelectorAll('button')
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
 const operators = ["%", "/", "*", "+", "-", "="];
-let output = '';
+let output = "";
 
-
-const calculate = (btnValue) =>{
-    if(btnValue === "=" && output !== ''){
-        output = eval(output.replace('%', '/100'))
-    } else if (btnValue === "AC"){
-        output = ''
-    } else if (btnValue === "DEL"){
-        output = output.slice(0,-1);
-    } else {
-        if(output === '' && operators.includes(btnValue)) return;
-        output += btnValue;
-    }
-    display.value = output;
+const calculate = (btnValue) => {
+  if (btnValue === "Enter") {
+    btnValue = "=";
+  }
+  if (btnValue === "=" && output !== "") {
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL" || btnValue === "Backspace") {
+    output = output.slice(0, -1);
+  } else {
+    if (output === "" && operators.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
 };
 
-buttons.forEach(button => {
-    button.addEventListener("click", e => calculate(e.target.innerText))
-})
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => calculate(e.target.innerText));
+});
+document.addEventListener("keypress", (e) => {
+  calculate(e.key);
+});
+document.addEventListener("keydown", (e) => {
+  return e.key == "Backspace" && calculate(e.key);
+});
