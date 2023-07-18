14e1724c62018396409f11367c156b38553f541b
Hasibur Rahman <hasibur.rahman@sazim.io>
2023-07-18 15:32:01 +0600

    fix keyboard functionality and multiple operators side by side problem

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 const operators = ["%", "/", "*", "+", "-", "="];
+const liKeys = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "0",
+  "Backspace",
+  "Enter",
+];
 let output = "";
 
 const calculate = (btnValue) => {
@@ -8,26 +22,26 @@ const calculate = (btnValue) => {
     btnValue = "=";
   }
   if (btnValue === "=" && output !== "") {
-    output = eval(output.replace("%", "/100"));
+    output = eval(output.replace("%", "/100")).toString();
   } else if (btnValue === "AC") {
     output = "";
   } else if (btnValue === "DEL" || btnValue === "Backspace") {
     output = output.toString().slice(0, -1);
   } else {
     if (output === "" && operators.includes(btnValue)) return;
-    if (operators.includes(btnValue) && operators.includes(output.at(-1)))
+    if (operators.includes(output.at(-1)) && operators.includes(btnValue))
       return;
     output += btnValue;
   }
   display.value = output;
+  console.log(output);
 };
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => calculate(e.target.innerText));
 });
-document.addEventListener("keypress", (e) => {
-  calculate(e.key);
-});
 document.addEventListener("keydown", (e) => {
-  return e.key == "Backspace" && calculate(e.key);
+  if (liKeys.includes(e.key) || operators.includes(e.key)) {
+    calculate(e.key);
+  }
 });
